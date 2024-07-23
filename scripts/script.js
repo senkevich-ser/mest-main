48c17d1684626b9f25c80ea1f704ef5117091c57
Siarhey Senkevich <accc75-mail.ru@yandex.ru>
2024-07-23 18:45:01 +0300

    added deleting cards & foto popup

--- scripts/script.js
+++ scripts/script.js
@@ -11,8 +11,8 @@ const popupName = popupDescription.querySelector(".popup__title");
 import { initialCards } from './initialCards.js';
 const cardContainer = document.querySelector(".foto-grid");
 const cardTemplate = document.querySelector(".foto-grid__template").content;
+const fotoOpenPopup = document.querySelector('.foto-open');
 
-console.log(formDescription)
 
 initialCards.forEach(el => { 
 cardContainer.append(addCard(el)); 
@@ -21,31 +21,47 @@ cardContainer.append(addCard(el));
 
 function addCard(dataCard){
   const card = cardTemplate.querySelector('.foto-grid__card').cloneNode(true);
-  const cardLink = card.querySelector(".foto-grid__item");
-  cardLink.src=dataCard.link
-  const cardName = card.querySelector(".foto-grid__name-title");
-  cardName.textContent=dataCard.name;
+  card.querySelector(".foto-grid__item").src=dataCard.link
+  card.querySelector(".foto-grid__name-title").textContent = dataCard.name;
+  const deleteBtn=card.querySelector(".foto-grid__urn");
+  deleteBtn.addEventListener('click',(evt)=>{
+  evt.target.parentElement.remove()
+  });
+  const cardImage = card.querySelector(".foto-grid__item");
+  cardImage.addEventListener('click',()=>{
+    openfotoOpenPopup(dataCard)
+    })
   return card;
 }
 
 function openProfilePopup() {
-  inputNameTitle.value = nameTitle.textContent;
-  inputDescriptionTitle.value = descriptionTitle.textContent;
+  inputNameTitle.placeholder = nameTitle.textContent;
+  inputDescriptionTitle.placeholder = descriptionTitle.textContent;
   popupDescription.classList.add("popup_opened");
   popupName.textContent='Редактировать профиль'
 }
 
 function openAddCardPopup() {
-  inputNameTitle.value = "Название";
-  inputDescriptionTitle.value = "Ссылка на картинку";
+  inputNameTitle.placeholder = "Название";
+  inputDescriptionTitle.placeholder = "Ссылка на картинку";
   popupDescription.classList.add("popup_opened");
   popupName.textContent='Новое место'
+}
+
+function openfotoOpenPopup(cardData) {
+  fotoOpenPopup.querySelector('.foto-open__image').src=cardData.link;
+  fotoOpenPopup.querySelector('.foto-open__name').textContent=cardData.name;
+  fotoOpenPopup.classList.add('popup_opened');
 }
 
 function closePopup(){
 popupDescription.classList.remove("popup_opened");
 formDescription.reset();
 }
+
+function closeFotoPopup(){
+  fotoOpenPopup.classList.remove("popup_opened");
+  }
 
 function handleFormProfileSubmit(evt) {
   nameTitle.textContent = inputNameTitle.value;
@@ -67,6 +83,7 @@ function handleFormAddCardSubmit(evt) {
 changeDescriptionBtn.addEventListener("click", openProfilePopup);
 addCardBtn.addEventListener("click", openAddCardPopup);
 closePopupBtn.addEventListener("click", closePopup);
+fotoOpenPopup.querySelector('.foto-open__cross').addEventListener("click", closeFotoPopup);
 formDescription.addEventListener("submit", (evt)=>{
   evt.preventDefault();
 if(evt.target.previousElementSibling.textContent==='Новое место'){
